a901bf11bc67de5cc36809ea421266f9f319815e
AKSHAY GIRME <akg7263@gmail.com>
2023-04-02 00:43:48 +0530

    Try Example for useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import UseReducerHook from './components/UseReducerHook';
 
 function App() {
   return (
     <div className="App">
-      WELCOME AKSHAY
+     <UseReducerHook />
           </div>
   );
 }
